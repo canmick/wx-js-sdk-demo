1b11d4d0a5d66200b4492052234ba476f9a3de34
candyman0753 <44766172+candyman0753@users.noreply.github.com>
2020-03-29 13:06:40 +0800

    add gzip

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,20 @@
 module.exports = {
   configureWebpack: config => {
-    if (process.env.NODE_ENV !== "production") {
-      config.devtool = "source-map";
+    if (process.env.NODE_ENV !== 'production') {
+      config.devtool = 'cheap-source-map'
     }
   },
   devServer: {
+    compress: true, // 开启gzip压缩
     disableHostCheck: true, // 绕过主机检查，解决Invalid Host header问题
     proxy: {
-      "/getSignature|checkout": {
+      '/getSignature|checkout': {
         // 本地项目api名称可以重写
         // pathRewrite: { "^/api": '/getSignature' }
         // 代理到本地node地址
-        target: "http://127.0.0.1:3000"
+        target: 'http://127.0.0.1:3000'
         // changeOrigin: true // 修改请求头host
       }
     }
   }
-};
+}
